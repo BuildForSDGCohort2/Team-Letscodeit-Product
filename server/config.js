3b8426f218a4553531c58de12c6052141914dac7
otitoju <otitojuoluwapelumi@gmail.com>
2020-08-21 15:45:21 +0100

    chores setup app server

---
+++ server/config.js
@@ -0,0 +1,6 @@
+export default {
+    "USER_SECRET": "qwertyuiocvbnmoiybn,.",
+    "DB_TEST": "mongodb://localhost:27017/FarmerConsumerTest",
+    "DB_DEV": "mongodb://localhost:27017/FarmerConsumer",
+    "DB_PROD": "mongodb://wordbox:wordboxpass1@ds225294.mlab.com:25294/FarmerConsumer"
+}
